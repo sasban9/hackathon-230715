5f141af539f96fccd343a37cc5a5de64118c2a1d
anees8 <aneesahmed8484@gmail.com>
2023-07-25 19:16:41 +0530

    Logout Page Design

---
+++ src/views/LogoutView.vue
@@ -0,0 +1,28 @@
+<template>
+   <BContainer fluid>
+        <BRow class="align-items-center justify-content-center vh-100 text-center">
+            <BCol>
+                
+    
+            <BSpinner> Logouting....</BSpinner>
+
+         
+        
+          </BCol>
+        </BRow>
+    </BContainer>
+  </template>
+  
+  <script setup>
+  import { onMounted } from "vue";
+  import { useLoginStore } from "@/stores/login.js";
+  
+  const { removeToken } = useLoginStore();
+  
+  onMounted(() => {
+    setTimeout(function () {
+      removeToken();
+    }, 500);
+  });
+  </script>
+  
