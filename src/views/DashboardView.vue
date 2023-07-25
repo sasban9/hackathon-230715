2dfef0dfd8f6685ec2c2785f67ef047d92729a1f
anees8 <aneesahmed8484@gmail.com>
2023-07-25 19:16:11 +0530

    dashboard page design

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,45 @@
+<template>
+  <BRow>
+  <BCol cols="12" md="6" lg="3">
+  <BCard no-body class="shadow-sm bg-white m-3" >
+   
+    <BRow>
+    <BCol cols="5" class="d-flex justify-content-center align-items-center text-center bg-success text-white"><h1>00</h1></BCol>
+    <BCol cols="7" class="d-flex justify-content-center align-items-center text-center p-3" style="min-height:100px"><BRow><BCol cols="12"><h5>Accepted Orders</h5></BCol></BRow></BCol>
+    </BRow>
+
+  </BCard>  
+</BCol>
+<BCol cols="12" md="6" lg="3">
+  <BCard no-body class="shadow-sm bg-white m-3" >
+   
+    <BRow>
+    <BCol cols="5" class="d-flex justify-content-center align-items-center text-center bg-dark text-white"><h1>00</h1></BCol>
+    <BCol cols="7" class="d-flex justify-content-center align-items-center text-center p-3" style="min-height:100px"><BRow><BCol cols="12"><h5>Total Orders</h5></BCol></BRow></BCol>
+    </BRow>
+
+  </BCard>  
+</BCol>
+<BCol cols="12" md="6" lg="3">
+  <BCard no-body class="shadow-sm bg-white m-3" >
+   
+    <BRow>
+    <BCol cols="5" class="d-flex justify-content-center align-items-center text-center bg-info  text-white"><h1>00</h1></BCol>
+    <BCol cols="7" class="d-flex justify-content-center align-items-center text-center p-3" style="min-height:100px"><BRow><BCol cols="12"><h5>Today Earning</h5></BCol></BRow></BCol>
+    </BRow>
+
+  </BCard>  
+</BCol>
+<BCol cols="12" md="6" lg="3">
+  <BCard no-body class="shadow-sm bg-white m-3" >
+   
+    <BRow>
+    <BCol cols="5" class="d-flex justify-content-center align-items-center text-center bg-warning  text-white"><h1>00</h1></BCol>
+    <BCol cols="7" class="d-flex justify-content-center align-items-center text-center p-3" style="min-height:100px"><BRow><BCol cols="12"><h5>Total Earning</h5></BCol></BRow></BCol>
+    </BRow>
+
+  </BCard>  
+</BCol>
+</BRow>
+</template>
+
