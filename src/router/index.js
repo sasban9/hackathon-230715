9331a73c4f93bc6994362fd98abc79e52d55d489
anees8 <aneesahmed8484@gmail.com>
2023-07-25 19:14:30 +0530

    Routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { useLoginStore } from "@/stores/login.js";
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 };
+},
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+      meta: {
+        requireAuth: false,
+    },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/logout',
+      name: 'logout',
+      component: () => import('../views/LogoutView.vue'),
+      meta: {
+        requireAuth: true,
+    },
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue'),
+      meta: {
+        requireAuth: true,
+    },
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: () => import('../views/OrdersView.vue'),
+      meta: {
+        requireAuth: true,
+    },
+    },
+
+    
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const { getAccessToken } = useLoginStore();
+  if (to.meta.requireAuth && getAccessToken === null) {
+      next({ name: "login" });
+  }
+  if (
+      to.name === "login" &&
+      !to.meta.requireAuth &&
+      getAccessToken !== null
+  ) {
+      next({ name: "dashboard" });
+  }
+  next();
+});
 
 export default router
